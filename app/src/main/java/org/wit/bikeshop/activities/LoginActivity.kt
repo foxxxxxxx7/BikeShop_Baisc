250340d38da727a539ad5020885e6ec98f1e14dd
Rob Fox <60361504+foxxxxxxx7@users.noreply.github.com>
2022-05-13 00:19:03 +0100

    edit activity_login.xml

--- app/src/main/java/org/wit/bikeshop/activities/LoginActivity.kt
+++ app/src/main/java/org/wit/bikeshop/activities/LoginActivity.kt
@@ -2,6 +2,7 @@ package org.wit.bikeshop.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.wit.bikeshop.R
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
