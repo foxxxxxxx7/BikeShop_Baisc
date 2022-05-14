a9ad9192303fb7affbed8124ead20703aa8613e1
Rob Fox <60361504+foxxxxxxx7@users.noreply.github.com>
2022-05-14 18:14:32 +0100

    Update BikeFirebase

--- app/src/main/java/org/wit/bikeshop/models/BikeFirebase.kt
+++ app/src/main/java/org/wit/bikeshop/models/BikeFirebase.kt
@@ -1,10 +1,14 @@
 package org.wit.bikeshop.models
 
+import com.google.firebase.database.FirebaseDatabase
 import org.jetbrains.anko.AnkoLogger
 
 /* > The BikeFirebase class is a Kotlin class that implements the BikeShopStore interface and the
 AnkoLogger interface */
 class BikeFirebase : BikeShopStore, AnkoLogger {
+    var bikes = mutableListOf<BikeModel>()
+    private val db = FirebaseDatabase.getInstance("https://bikeshop-basic-default-rtdb.europe-west1.firebasedatabase.app/").getReference()
+
     override fun findAll(): List<BikeModel> {
         TODO("Not yet implemented")
     }
